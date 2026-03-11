c3d7e39cb1a62f06b6b6d070e49a5e059f5f67b5
Leen-A-Saleh <leenanas412@gmail.com>
2026-03-11 18:36:59 +0300

    Add javaScript tasks

---
+++ JavaScript Tasks/assets/js/index.js
@@ -0,0 +1,20 @@
+// let num1 = Number(prompt("Enter the first number:"));
+// let op = prompt("Enter the operation:");
+// let num2 = Number(prompt("Enter the secound number:"));
+// let res;
+
+// function calculat(num1, op, num2) {
+//     if (op == "+") {
+//         return num1 + num2;
+//     } else if (op == "-") {
+//         return num1 - num2;
+//     } else if (op == "*") {
+//         return num1 * num2;
+//     } else if (op == "/") {
+//         return num1 / num2;
+//     } else {
+//         return " ";
+//     }
+// }
+// res = calculat(num1, op, num2);
+// document.querySelector("input").value = `${num1} ${op} ${num2} = ${res}`;
