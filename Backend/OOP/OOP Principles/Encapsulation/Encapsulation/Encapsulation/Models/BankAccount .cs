0f3922159174364644953ce8ece3bf47de1364d3
Leen-A-Saleh <leenanas412@gmail.com>
2026-04-25 01:04:56 +0300

    Encapsulation

---
+++ Backend/OOP/OOP Principles/Encapsulation/Encapsulation/Encapsulation/Models/BankAccount .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Models
+{
+    internal class BankAccount
+    {
+        private double Balance;
+        public BankAccount(double balance)
+        {
+             this.Balance = balance;
+        }
+        public void Deposit(double balance)
+        {
+            Balance += balance;
+            Console.WriteLine($"The deposit was successful.\n Deposite: {balance}. New Balance: {Balance}");
+        }
+        public void Withdraw(double balance)
+        {
+            Balance -= balance;
+            Console.WriteLine($"The withdraw was successful.\n Withdraw: {balance}. New Balance: {Balance}");
+        }
+        override public string ToString()
+        {
+            return $"Balance: {Balance}";
+        }
+    }
+}
---
+++ Backend/OOP/OOP Principles/Encapsulation/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using Encapsulation.Models;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount user1 = new BankAccount(1000000);
+            user1.Deposit(500);
+            user1.Withdraw(500000);
+            Console.WriteLine(user1);
+
+        }
+    }
+}
